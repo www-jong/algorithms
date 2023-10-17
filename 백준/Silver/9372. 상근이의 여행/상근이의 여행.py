132a588328117987d7019646de8db6fc9fd50000
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 13:33:51 +0900

    [Silver IV] Title: 상근이의 여행, Time: 188 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,6 @@
+import sys
+for i in range(int(sys.stdin.readline())):
+    n,m=map(int,sys.stdin.readline().split())
+    for _ in range(m):
+        x,y=map(int,sys.stdin.readline().split())
+    print(n-1)
