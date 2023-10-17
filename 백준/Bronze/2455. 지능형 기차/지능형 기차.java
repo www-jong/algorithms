1c6533f1a29a931a02e4d504d4fb6461eee17e5f
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 13:04:48 +0900

    [Bronze III] Title: 지능형 기차, Time: 128 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int count=0;
+		int max=0;
+		int c=0;
+		while(true) {
+		int[] A=Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		count=count-A[0]+A[1];
+		if(count>max) {max=count;}
+		c++;
+		if(c==4) {break;}
+		}
+		br.close();
+		System.out.println(max);
+	
+	}
+
+}
