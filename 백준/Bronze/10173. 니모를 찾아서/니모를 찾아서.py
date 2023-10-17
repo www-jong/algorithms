d70ec350cde075d2eac68d9d1dc7001cb1e8a878
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 13:37:56 +0900

    [Bronze II] Title: 니모를 찾아서, Time: 68 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Bronze/10173. 니모를 찾아서/니모를 찾아서.py
@@ -0,0 +1,9 @@
+while True:
+    arr=input()
+    arrr=arr.lower()
+    if arrr.find('nemo') != -1:
+        print("Found")
+    elif "eoi" in arrr and "EOI" in arr:
+        break
+    else:
+        print("Missing")
