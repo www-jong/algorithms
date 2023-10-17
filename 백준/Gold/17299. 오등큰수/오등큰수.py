114c5ba3f22c063756f043ce45eca49eae876447
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 14:11:37 +0900

    [Gold III] Title: 오등큰수, Time: 1432 ms, Memory: 155576 KB -BaekjoonHub

---
+++ 백준/Gold/17299. 오등큰수/오등큰수.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+N=int(input())
+li=list(map(int,input().split()))
+li2=[0]*(max(li)+1)
+res=[-1]*(len(li))
+stk=[]
+for i in li:
+    li2[i]+=1
+for i in range(N):
+    while stk and li2[li[stk[-1]]]<li2[li[i]]:
+        res[stk.pop()]=li[i]
+    stk.append(i)
+print(*res)
