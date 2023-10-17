19db37577ba66f2a8aada13a6c89f6e4472541a0
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 12:51:28 +0900

    [Gold III] Title: 소수의 연속합, Time: 1884 ms, Memory: 93756 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -0,0 +1,33 @@
+N=int(input())
+check=[0,0]+[1]*(N-1)
+primes=[]
+
+for i in range(2,N+1):
+    if check[i]:
+        primes.append(i)
+        for j in range(2*i,N+1,i):
+            check[j]=0
+if N!=1:
+    st=0
+    end=0
+    sums=primes[st]
+    res=0
+    while end<len(primes):
+        if sums==N:
+            res+=1
+            sums-=primes[st]
+            st+=1
+        elif sums<N:
+            if end<len(primes)-1:
+                end+=1
+                sums+=primes[end]
+            else:
+                break
+        else:
+            if st==end:
+                break
+            sums-=primes[st]
+            st+=1
+    print(res)
+else:
+    print(0)
