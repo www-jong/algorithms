12e5764c6b09fd1d0274b70b97bbf7e18affee16
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 13:06:06 +0900

    [Silver IV] Title: 가로수, Time: 120 ms, Memory: 38964 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+N=int(input())
+def gcd(a,b):
+    while b>0:
+        a,b=b,a%b
+    return a
+li=[]
+res=0
+for i in range(N):
+    li.append(int(input()))
+li.sort()
+gcds=[]
+for i in range(1,N):
+    gcds.append(li[i]-li[i-1])
+gcd_val=li[1]-li[0]
+for i in gcds:
+    gcd_val=gcd(gcd_val,i)
+print((li[-1]-li[0])//gcd_val-(N-1))
