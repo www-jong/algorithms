b47505e07a94711bb7e402d6deafee21bb791fe4
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 13:16:42 +0900

    [Gold IV] Title: 검문, Time: 2360 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Gold/2981. 검문/검문.py
@@ -0,0 +1,20 @@
+import math
+li=[]
+for i in range(int(input())):
+    li.append(int(input()))
+li.sort(reverse=True)
+li2=[li[0]-li[1]]
+for i in range(1,len(li)-1):
+    li2.append(li[i]-li[i+1])
+mins=min(li)
+gcds=math.gcd(*li2)
+ans=[gcds]
+for i in range(2,gcds//2+1):
+    if gcds%i==0:
+        ans.append(gcds//i)
+ans.sort()
+print(*ans)
+
+'''
+유클리드 호제법
+'''
