1cefbb62ca4af33497a7ab025ffe88bbecd48b0b
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 13:51:21 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 168 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,8 @@
+n=int(input())
+li=list(map(int,input().split()))
+dp=[1]*(n+1)
+for i in range(1,n):
+    for j in range(i):
+        if li[i]<li[j]:
+            dp[i]=max(dp[i],dp[j]+1)
+print(max(dp))
