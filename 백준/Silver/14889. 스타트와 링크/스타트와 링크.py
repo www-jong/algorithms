7fcec3ecc24d44498b6e7c8328e2b5e76ec7f0cc
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 14:01:28 +0900

    [Silver I] Title: 스타트와 링크, Time: 1596 ms, Memory: 149096 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,36 @@
+from itertools import combinations
+n=int(input())
+allmember=[i for i in range(1,n+1)]
+pick=[]
+temp=[]
+
+for i in combinations(allmember,n//2):
+    pick.append(list(i))
+
+def getpower(power,memberlist):
+    for i,j in memberlist:
+        power+=li[i][j]+li[j][i]
+    return power
+
+li=[[0]*(n+1)]
+for i in range(n):
+    li.append([0]+list(map(int,input().split())))
+
+powergap=1000000
+for bluemember in pick:
+    gap=0
+    blueteam=[]
+    redteam=[]
+    bpower=0
+    rpower=0
+    for i in combinations(bluemember,2):
+        blueteam.append(list(i))
+    redmember=list(set(allmember)-set(bluemember))
+    for i in combinations(redmember,2):
+        redteam.append(list(i))
+    bpower=getpower(bpower,blueteam)
+    rpower=getpower(rpower,redteam)
+    gap=abs(bpower-rpower)
+    if gap<powergap:
+        powergap=gap
+print(powergap)
