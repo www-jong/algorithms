69f9436f5da650ee33db5e423ae360f89e181a6a
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 13:25:59 +0900

    [Silver I] Title: 카잉 달력, Time: 1628 ms, Memory: 204192 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -0,0 +1,15 @@
+for i in range(int(input())):
+    M,N,x,y=map(int,input().split())
+    res=-1
+    dic={}
+    idx=0
+    while M*idx+x<=M*N:
+        dic[M*idx+x]=1
+        idx+=1
+    idx=0
+    while N*idx+y<=M*N:
+        if N*idx+y in dic:
+            res=N*idx+y
+            break
+        idx+=1
+    print(res)
