e00862450643b70fe5a401ff9c64723fb4e310bb
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 13:19:42 +0900

    [Gold II] Title: 친구 네트워크, Time: 344 ms, Memory: 61296 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -0,0 +1,35 @@
+import sys
+input=sys.stdin.readline
+sys.setrecursionlimit(1000000)
+
+def find(a):
+    if a==nodes[a]:
+        return a
+    nodes[a]=find(nodes[a])
+    return nodes[a]
+
+def find_count(a,c):
+    if a==nodes[a]:
+        return c
+    find_count(nodes[a],c+1)
+
+def union(a,b):
+    a=find(a)
+    b=find(b)
+    if a!=b:
+        nodes[b]=a
+        counts[a]+=counts[b]
+for i in range(int(input())):
+    F=int(input())
+    nodes={}
+    counts={}
+    for i in range(F):
+        a,b=map(str,input().split())
+        if a not in nodes:
+            nodes[a]=a
+            counts[a]=1
+        if b not in nodes:
+            nodes[b]=b
+            counts[b]=1
+        union(a,b)
+        print(counts[find(a)])
