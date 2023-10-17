b66fcd8ee166ef3549353695ca0a2eb22c895c95
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 13:36:05 +0900

    [Gold IV] Title: 문자열 폭발, Time: 632 ms, Memory: 49272 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+S=list(input().rstrip())
+bomb=list(input().rstrip())
+len_bomb=len(bomb)
+stack=[]
+idx=0
+while idx!=len(S):
+    stack.append(S[idx])
+    if stack[-1]==bomb[-1]:
+        if stack[-len_bomb:]==bomb:
+            for i in range(len_bomb):
+                stack.pop()
+    idx+=1
+
+if stack:
+    print(''.join(stack))
+else:
+    print("FRULA")
