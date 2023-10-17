16a328d75db773b5c07c4fabdb44f6c6c72b485a
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-10-17 13:16:25 +0900

    [Bronze II] Title: 창영이의 일기장, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2954. 창영이의 일기장/창영이의 일기장.py
@@ -0,0 +1,4 @@
+n=input()
+n=n.replace("apa","ㅁ").replace("epe","ㄷ").replace("ipi","ㅑ").replace("opo","ㅐ").replace("upu","ㅕ")
+n=n.replace("ㅁ","a").replace("ㄷ","e").replace("ㅑ","i").replace("ㅐ","o").replace("ㅕ","u")
+print(n)
