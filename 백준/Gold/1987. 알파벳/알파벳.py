95ff003c027d179851052c3536098ab795cb6253
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2023-12-27 09:05:54 +0900

    [Gold IV] Title: 알파벳, Time: 4404 ms, Memory: 142164 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,24 +1,18 @@
 import sys
-count=1
-def bfs(x,y):
-    q=set()
-    q.add((x,y,graph[x][y]))
-    global count
-    while q:
-        x,y,ans=q.pop()
-        for i in range(4):
-            sx=x+dx[i]
-            sy=y+dy[i]
-            if 1<=sx<=r and 1<=sy<=c and graph[sx][sy] not in ans:
-                q.add((sx,sy,ans+graph[sx][sy]))
-                count=max(len(ans)+1,count)
-r,c=map(int,sys.stdin.readline().split())
-graph=[['0']*(c+1)]
-for i in range(r):
-    graph.append(['0']+list(sys.stdin.readline()))
-visits=[[0]*(c+1) for i in range(r+1)]
-dx=[0,0,1,-1]
-dy=[1,-1,0,0]
-bfs(1,1)
-print(count)
+d=[0,0,1,-1]
+R,C=map(int,input().split())
+li=[input() for _ in range(R)]
+dic={}
+res=0
+def dfs(now,cnt,visit):
+    global res
+    res=max(res,cnt)
+    if visit==(1<<26)-1 or res==26:
+        return
+    for i in range(4):
+        nx,ny=now[0]+d[i],now[1]+d[3-i]
+        if 0<=nx<R and 0<=ny<C and not visit&(1<<ord(li[nx][ny])-65):
+            dfs((nx,ny),cnt+1,visit|(1<<(ord(li[nx][ny])-65)))
 
+dfs((0,0),1,(1<<ord(li[0][0])-65))
+print(res)
