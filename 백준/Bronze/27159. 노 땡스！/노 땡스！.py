f8c9d50ba145c4989c37175aa9866c0f280da94f
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2024-01-01 17:41:48 +0900

    [Bronze III] Title: 노 땡스!, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27159. 노 땡스！/노 땡스！.py
@@ -0,0 +1,11 @@
+N=int(input())
+li=list(map(int,input().split()))+[10**9]
+res=0
+tmp=[0]
+for i in range(len(li)):
+    if tmp[-1]-1!=li[i] and li[i]!=tmp[-1]+1:
+        res+=min(tmp)
+        tmp=[li[i]]
+    else:
+        tmp.append(li[i])
+print(res)
