938495da7c18453dc85df5d9617aa92ad62f722e
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2024-02-07 01:36:50 +0900

    [Gold IV] Title: 좋다, Time: 488 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -0,0 +1,16 @@
+N=input()
+li=list(map(int,input().split()))
+dic={}
+for i in li:
+    if i not in dic:
+        dic[i]=1
+    else:
+        dic[i]+=1
+r=0
+for i in li:
+    for j in dic:
+        if i-j in dic:
+            if not((j==(i-j) and dic[j]<=2) or(i==j and dic[i]==1)or(i==(i-j)and dic[i]==1)):
+                r+=1
+                break
+print(r)
