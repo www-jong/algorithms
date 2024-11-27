fa1f5b8984156ef649d0a3e8a9ad93c01deda095
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2024-11-27 17:52:59 +0900

    [Gold V] Title: 토마토, Time: 3416 ms, Memory: 64488 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,45 +1,33 @@
-from collections import deque
-import copy
-n,m,h=map(int,input().split())
-graph=[[3]]
-for j in range(h):
-    ssss=[[3]*(n+1)]
-    for i in range(m):
-        ssss.append([3]+list(map(int,input().split())))
-    graph.append(ssss)
-dx=[0,0,1,-1,0,0]
-dy=[1,-1,0,0,0,0]
-dz=[0,0,0,0,1,-1]
-d=deque()
-count=0
-for aa in range(1,1+m):
-    for bb in range(1,1+n):
-        for cc in range(1,1+h):
-            if graph[cc][aa][bb]==1:
-                d.append((cc,aa,bb))
-            else:continue
-while d:
-    q=copy.deepcopy(d)
-    d.clear()
-    while q:
-        z,x,y=q.popleft()
-        for i in range(6):
-            sz=z+dz[i]
-            sx=x+dx[i]
-            sy=y+dy[i]
-            if 1<=sx<=m and 1<=sy<=n and 1<=sz<=h:
-                if graph[sz][sx][sy]==0:
-                    graph[sz][sx][sy]=1
-                    d.append((sz,sx,sy))
-    count+=1
-ss=0
-for zz in range(1,h+1):
-    for xx in range(1,1+m):
-        for yy in range(1,1+n):
-            if 0==graph[zz][xx][yy] and ss==0:
-                print(-1)
-                ss=1
-                break
-            else:continue
-if ss==0:
-    print(count-1)
+import sys
+input=sys.stdin.readline
+d=[[1,0,0],[-1,0,0],[0,1,0],[0,-1,0],[0,0,1],[0,0,-1]]#hnm
+
+M,N,H=map(int,input().split())
+li=[[] for _ in range(H)]
+q=[]
+ch=0
+for h in range(H):
+    for n in range(N):
+        tmp=list(map(int,input().split()))
+        for m in range(M):
+            if tmp[m]==1:q.append((h,n,m))
+            elif tmp[m]==0:ch+=1
+        li[h].append(tmp)
+
+res=0
+while q:
+    tos=q
+    q=[]
+    flag=0
+    for now in tos:
+        for i in d:
+            next=[now[0]+i[0],now[1]+i[1],now[2]+i[2]]
+            if 0<=next[0]<H and 0<=next[1]<N and 0<=next[2]<M:
+                if li[next[0]][next[1]][next[2]]==0:
+                    flag=1
+                    li[next[0]][next[1]][next[2]]=1
+                    ch-=1
+                    q.append(next)
+    if flag:
+        res+=1
+print(-1 if ch else res)
