fda16f481fd268707330d16c0a02d6bf33abb65d
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2024-11-27 19:39:52 +0900

    [Gold V] Title: 농장 관리, Time: 72 ms, Memory: 34104 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.py
+++ 백준/Gold/1245. 농장 관리/농장 관리.py
@@ -3,38 +3,32 @@ def check(x,y):
     return 0<=x<N and 0<=y<M
 N,M=map(int,input().split())
 d=[[0,1],[0,-1],[1,0],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]]
-
 li=[list(map(int,input().split())) for _ in range(N)]
 visit=[[0]*(M) for _ in range(N)]
 res=0
 for i in range(N):
     for j in range(M):
         if not visit[i][j]:
-            bong=[(i,j)]
+            tmp=[(i,j)]
             q=deque()
             q.append((i,j))
-            while q:
-                x,y=q.popleft()
-                for k in range(8):
-                    nx,ny=x+d[k][0],y+d[k][1]
-                    if check(nx,ny) and not visit[nx][ny] and (nx,ny) not in bong and li[x][y]==li[nx][ny]:
-                        bong.append((nx,ny))
-                        q.append((nx,ny))
             flag=1
-            q=deque(bong)
             while q:
                 x,y=q.popleft()
                 for k in range(8):
                     nx,ny=x+d[k][0],y+d[k][1]
-                    if check(nx,ny):
-                        if visit[nx][ny] or([nx,ny] not in bong and li[x][y]<li[nx][ny]):
+                    if check(nx,ny) and (nx,ny) not in tmp:
+                        if li[i][j]==li[nx][ny]:
+                            q.append((nx,ny))
+                            tmp.append((nx,ny))
+                        elif li[i][j]<li[nx][ny]:
                             flag=0
                             break
                 if not flag:
                     break
             if flag:
                 res+=1
-                for x,y in bong:
+                for x,y in tmp:
                     visit[x][y]=1
 
 print(res)
