9693b7e254dfd4625e24c4821affeb73cbc34af0
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2025-02-05 13:59:39 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 32412 KB, Score: 20 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -4,16 +4,14 @@ input=sys.stdin.readline
 N,K=map(int,input().split())
 li=[list(map(int,input().split()))for _ in range(N)]
 li.sort(key=lambda x:(-x[1],-x[2],-x[3]))
-
-cnt=1
+cnt=0
 tmp=[-1,-1,-1]
 a=0
 while a<N:
-    if tmp[0]!=li[a][1] and tmp[1]!=li[a][2] and tmp[2]!=li[a][3]:
+    if tmp[0]!=li[a][1] or tmp[1]!=li[a][2] or tmp[2]!=li[a][3]:
         cnt+=1
         tmp=li[a][1:]
-    else:
-        pass
     if li[a][0]==K:
         print(cnt)
+        break
     a+=1
