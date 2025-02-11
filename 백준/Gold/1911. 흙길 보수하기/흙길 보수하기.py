c0048bcb1f16b97041fccf5c3234656d10c66113
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2025-02-11 17:53:14 +0900

    [Gold V] Title: 흙길 보수하기, Time: 52 ms, Memory: 36584 KB -BaekjoonHub

---
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
@@ -0,0 +1,22 @@
+import sys,math
+input=sys.stdin.readline
+N,L=map(int,input().split())
+li=[]
+for i in range(N):
+    a,b=map(int,input().split())
+    li.append((a,b-1))
+li.sort(key=lambda x:(x[0],-x[1]))
+res=0
+now=0
+for a,b in li:
+    if b<=now:
+        continue
+    elif now<a:
+        val=math.ceil((b-a+1)/L)
+        res+=val
+        now=a+val*L-1
+    elif a<=now and now<b:
+        val=math.ceil((b-now)/L)
+        res+=val
+        now=now+val*L
+print(res)
