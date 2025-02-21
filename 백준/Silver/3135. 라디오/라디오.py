18bf331536ce38b04e9005ea50206ca4e7becdb6
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2025-02-21 16:20:07 +0900

    [Silver V] Title: 라디오, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3135. 라디오/라디오.py
+++ 백준/Silver/3135. 라디오/라디오.py
@@ -1,11 +1,10 @@
 a,b=map(int,input().split())
 N=int(input())
 li=[int(input()) for i in range(N)]
-tmp1,tmp2=[],[]
-tmp1.append(abs(a-b))
+tmp=[]
 for i in range(len(li)):
-    tmp2.append(abs(li[i]-b))
-if abs(a-b)<=min(tmp2):
+    tmp.append(abs(li[i]-b))
+if abs(a-b)<=min(tmp):
     print(abs(a-b))
 else:
-    print(min(tmp2)+1)
+    print(min(tmp)+1)
