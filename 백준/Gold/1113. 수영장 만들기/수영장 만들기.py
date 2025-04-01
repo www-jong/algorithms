1617753f5c722f9b09e35cd6f4f477bc39131612
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2025-04-01 18:19:54 +0900

    [Gold I] Title: 수영장 만들기, Time: 104 ms, Memory: 35044 KB -BaekjoonHub

---
+++ 백준/Gold/1113. 수영장 만들기/수영장 만들기.py
@@ -0,0 +1,48 @@
+from collections import deque
+d=[0,0,1,-1]
+N,M=map(int,input().split())
+li=[input() for _ in range(N)]
+maxh=max([max([int(j) for j in i]) for i in li])
+res=0
+flood=[[0]*(M) for _ in range(N)] #1이면 물새는지역
+
+
+
+for h in range(1,maxh):
+    visit=[[0]*(M) for _ in range(N)]
+    for x in range(N):
+        for y in range(M):
+            if (x==0 or x==N-1 or y==0 or y==M-1) and int(li[x][y])<=h:
+                flood[x][y]=1
+    for x in range(N):
+        for y in range(M):
+            flag=1
+            flood_list=[]
+            cnt=0
+
+            if not visit[x][y] and not flood[x][y] and int(li[x][y])<=h:
+                q=deque([])
+                q.append((x,y))
+                flood_list.append((x,y))
+                visit[x][y]=1
+                cnt+=1
+                while q:
+                    nx,ny=q.popleft()
+                    chk=0
+                    for i in range(4):
+                        dx,dy=nx+d[i],ny+d[3-i]
+                        if 0<=dx<N and 0<=dy<M:
+                            if not visit[dx][dy]:
+                                if not flood[dx][dy] and int(li[dx][dy])<=h:
+                                    q.append((dx,dy))
+                                    flood_list.append((dx,dy))
+                                    visit[dx][dy]=1
+                                    cnt+=1
+                                elif flood[dx][dy]:
+                                    flag=0
+                if flag:
+                    res+=cnt
+                else:
+                    for fx,fy in flood_list:
+                        flood[fx][fy]=1
+print(res)
