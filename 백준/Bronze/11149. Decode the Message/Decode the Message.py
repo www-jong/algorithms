487f183bbdce5494ef08c8737a5c750b61afcd4a
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2025-04-01 21:22:53 +0900

    [Bronze II] Title: Decode the Message, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11149. Decode the Message/Decode the Message.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    S=input().split()
+    r=[]
+    for i in S:
+        t=0
+        for j in i:
+            t+=ord(j)-97
+        r.append(t%27)
+    r=''.join([chr(i+97) if 0<=i<26 else ' ' for i in r])
+    print(r)
