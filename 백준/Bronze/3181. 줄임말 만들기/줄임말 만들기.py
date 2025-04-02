8652219cb55b4861bad69afe5c7c33fc72ea120d
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2025-04-02 18:37:38 +0900

    [Bronze II] Title: 줄임말 만들기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/3181. 줄임말 만들기/줄임말 만들기.py
@@ -0,0 +1,10 @@
+s=['i', 'pa', 'te', 'ni', 'niti', 'a', 'ali', 'nego', 'no', 'ili']
+S=input().split()
+t=S[0][0].upper()
+for i in S[1:]:
+    for j in s:
+        if i==j:
+            break
+    else:
+        t+=i[0].upper()
+print(t)
