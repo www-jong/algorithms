ac8d58d16b10821627edc6e5eeed2e4457fa88fd
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2025-06-23 20:01:46 +0900

    [Gold III] Title: 방 번호, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1082. 방 번호/방 번호.py
+++ 백준/Gold/1082. 방 번호/방 번호.py
@@ -1,10 +1,5 @@
-N=int(input())
-P=list(map(int,input().split()))
-M=int(input())
-dp=[-1]*(M+1)
-
+I=input;N=int(I());P=[*map(int,I().split())];M=int(I());d=[-1]*(M+1)
 for i in range(N-1,-1,-1):
-    now=P[i]
-    for j in range(now,M+1):
-        dp[j]=max(dp[j-now]*10+i,i,dp[j])
-print(dp[M])
+ for j in range(P[i],M+1):
+  d[j]=max(d[j-P[i]]*10+i,i,d[j])
+print(d[M])
