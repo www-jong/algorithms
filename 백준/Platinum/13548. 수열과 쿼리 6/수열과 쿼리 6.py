c399330ec788558ba99ea983ae15aa41252192f0
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2025-10-30 18:03:23 +0900

    [Platinum I] Title: 수열과 쿼리 6, Time: 2696 ms, Memory: 182424 KB -BaekjoonHub

--- 백준/Platinum/13548. 수열과 쿼리 6/수열과 쿼리 6.py
+++ 백준/Platinum/13548. 수열과 쿼리 6/수열과 쿼리 6.py
@@ -13,44 +13,39 @@ for idx in range(M):
 query.sort(key=lambda x:(x[0],x[2]))
 
 le,ri=0,-1
-dk=defaultdict(int)
-dv=defaultdict(set)
-#for i in li:
-#    dk[i]+=1
-cnt=0
-#for k,v in dk.items():
-#    dv[v].add(k)
-#    cnt=max(v,cnt)
+cnt=defaultdict(int)
+dv=[0]*(M+5)
+val=0
 res=[0]*M
 for _,next_le,next_ri,idx in query:
     while le<next_le:
-        dv[dk[li[le]]].discard(li[le])
-        if dk[li[le]]==cnt and not dv[dk[li[le]]]:
-            cnt-=1
-        dv[dk[li[le]]-1].add(li[le])
-        dk[li[le]]-=1
+        dv[cnt[li[le]]]-=1
+        if dv[cnt[li[le]]]==0 and cnt[li[le]]==val:
+            val-=1
+        dv[cnt[li[le]]-1]+=1
+        cnt[li[le]]-=1
         le+=1
 
     while next_le<le:
         le-=1
-        dv[dk[li[le]]].remove(li[le])
-        dv[dk[li[le]]+1].add(li[le])
-        cnt=max(dk[li[le]]+1,cnt)
-        dk[li[le]]+=1
+        dv[cnt[li[le]]]-=1
+        dv[cnt[li[le]]+1]+=1
+        val=max(cnt[li[le]]+1,val)
+        cnt[li[le]]+=1
 
     while ri<next_ri:
         ri+=1
-        dv[dk[li[ri]]].discard(li[ri])
-        dv[dk[li[ri]]+1].add(li[ri])
-        cnt=max(dk[li[ri]]+1,cnt)
-        dk[li[ri]]+=1
+        dv[cnt[li[ri]]]-=1
+        dv[cnt[li[ri]]+1]+=1
+        val=max(cnt[li[ri]]+1,val)
+        cnt[li[ri]]+=1
     while next_ri<ri:
-        dv[dk[li[ri]]].remove(li[ri])
-        if dk[li[ri]]==cnt and not dv[dk[li[ri]]]:
-            cnt-=1
-        dv[dk[li[ri]]-1].add(li[ri])
-        dk[li[ri]]-=1
+        dv[cnt[li[ri]]]-=1
+        if dv[cnt[li[ri]]]==0 and cnt[li[ri]]==val:
+            val-=1
+        dv[cnt[li[ri]]-1]+=1
+        cnt[li[ri]]-=1
         ri-=1
-    res[idx]=cnt
+    res[idx]=val
 
 print(*res,sep="\n")
