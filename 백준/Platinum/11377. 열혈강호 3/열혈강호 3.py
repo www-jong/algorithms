778560d447167de25695e6cc5fdeb1fe2c74eb16
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2025-11-06 19:04:33 +0900

    [Platinum III] Title: 열혈강호 3, Time: 408 ms, Memory: 117392 KB -BaekjoonHub

--- 백준/Platinum/11377. 열혈강호 3/열혈강호 3.py
+++ 백준/Platinum/11377. 열혈강호 3/열혈강호 3.py
@@ -1,60 +1,37 @@
-from collections import deque
 import sys
 input=sys.stdin.readline
 
+def func(x):
+    global t
+    if visit[x]:
+        return 0
+    visit[x]=1
+    for i in li[x]:
+        if check[i]==-1 or func(check[i]):
+            check[i]=x
+            return 1
+    return 0
+
 N,M,K=map(int,input().split())
-graph=[[] for _ in range(N+M+3)]
-capacity=[[0]*(N+M+3) for _ in range(N+M+3)]
-flow=[[0]*(N+M+3) for _ in range(N+M+3)]
+li=[]
+check=[-1]*(M+1)
+res1,res2=0,0
+for _ in range(N):
+    n,*tmp=map(int,input().split())
+    li.append(tmp)
 
-source=0
-sink=0+N+M+1
-branch=0+N+M+2
-for i in range(1,N+1):
-    graph[0].append(i)
-    graph[i].append(0)
-    capacity[0][i]=1
-    graph[branch].append(i)
-    graph[i].append(branch)
-    capacity[branch][i]=1
-graph[0].append(branch)
-graph[branch].append(0)
-capacity[0][branch]=K
-for i in range(N+1,N+M+2):
-    graph[i].append(sink)
-    graph[sink].append(i)
-    capacity[i][sink]=1
-
-for i in range(1,N+1):
-    _,*li=list(map(int,input().split()))
-    for j in li:
-        graph[i].append(N+j)
-        graph[N+j].append(i)
-        capacity[i][N+j]=1
-
-def bfs(source,sink,visit):
-    q=deque()
-    q.append(source)
-    while q and visit[sink]==-1:
-        a=q.popleft()
-        for b in graph[a]:
-            if visit[b]==-1 and capacity[a][b]-flow[a][b]>0:
-                q.append(b)
-                visit[b]=a
-                if b==sink:
-                    break
-    return 1 if visit[sink]!=-1 else 0
-
-res=0
-while True:
-    visit=[-1]*(N+M+3)
-    if not bfs(source,sink,visit):
+for i in range(N):
+    visit=[0]*N
+    if func(i):
+        res1+=1
+    if res1==M:
         break
-    b=sink
-    while b!=source:
-        a=visit[b]
-        flow[a][b]+=1
-        flow[b][a]-=1
-        b=a
-    res+=1
-print(res)
+
+for i in range(N):
+    visit=[0]*N
+    if func(i):
+        res2+=1
+    if res2==K or res1+res2==M:
+        break
+
+print(res1+res2)
