1b663bf66634ea3b3a36b8c0ef22c23d4d4006a3
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2026-01-07 18:48:20 +0900

    [Gold I] Title: 연결, Time: 72 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/5022. 연결/연결.py
+++ 백준/Gold/5022. 연결/연결.py
@@ -2,28 +2,24 @@ import sys
 N,M,*L=map(int,sys.stdin.read().split())
 A,B,C,D=L[:2],L[2:4],L[4:6],L[6:8]
 R=range
-
 def K(s,e,v):
-    q=[(*s,0)];v[s[0]][s[1]]=1
-    for x,y,c in q:
-        if[x,y]==e:return c
-        for i in R(4):
-            X,Y=x+[0,0,1,-1][i],y+[1,-1,0,0][i]
-            if 0<=X<=N and 0<=Y<=M and not v[X][Y]:
-                v[X][Y]=x,y;q+=[(X,Y,c+1)]
-    return 1e5
-
+ q=[(*s,0)];v[s[0]][s[1]]=1
+ for x,y,c in q:
+  if[x,y]==e:return c
+  for i in R(4):
+   X,Y=x+[0,0,1,-1][i],y+[1,-1,0,0][i]
+   if 0<=X<=N and 0<=Y<=M and not v[X][Y]:v[X][Y]=x,y;q+=[(X,Y,c+1)]
+ return 1e5
 def f(a,b,c,d):
-    v=[[0]*(M+1)for _ in R(N+1)]
-    v[c[0]][c[1]]=v[d[0]][d[1]]=1
-    if(l:=K(a,b,v))>9e4:return 1e5
-    V=[[0]*(M+1)for _ in R(N+1)];x,y=b
-    while 1:
-        V[x][y]=1
-        if[x,y]==a:break
-        x,y=v[x][y]
-    return l+K(c,d,V)
-
+ v=[[0]*(M+1)for _ in R(N+1)]
+ v[c[0]][c[1]]=v[d[0]][d[1]]=1
+ if(l:=K(a,b,v))>9e4:return 1e5
+ V=[[0]*(M+1)for _ in R(N+1)];x,y=b
+ while 1:
+  V[x][y]=1
+  if[x,y]==a:break
+  x,y=v[x][y]
+ return l+K(c,d,V)
 a,b=f(A,B,C,D),f(C,D,A,B)
 r=min(a,b)
 print(r if r<1e5 else"IMPOSSIBLE")
