c7d30f2837bdedbf87752aae6c89cba6dee1a809
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2026-01-07 18:46:30 +0900

    [Gold I] Title: 연결, Time: 72 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/5022. 연결/연결.py
+++ 백준/Gold/5022. 연결/연결.py
@@ -1,47 +1,29 @@
-from collections import deque
 import sys
-input=sys.stdin.readline
-d=[0,0,1,-1]
+N,M,*L=map(int,sys.stdin.read().split())
+A,B,C,D=L[:2],L[2:4],L[4:6],L[6:8]
+R=range
 
-def bfs(st,end,visit):
-    q=deque([[*st,0]])
-    while q:
-        x,y,c=q.popleft()
-        for i in range(4):
-            nx,ny=x+d[i],y+d[3-i]
-            if 0<=nx<=N and 0<=ny<=M and not visit[nx][ny]:
-                q.append([nx,ny,c+1])
-                visit[nx][ny]=[x,y]
-                if [nx,ny]==end:
-                    return c+1
-    return -1
-def func(a1,a2,b1,b2):
-    res=0
-    visit=[[0]*(M+1) for _ in range(N+1)]
-    for x,y in [b1,b2]:
-        visit[x][y]=1
-    cost=bfs(a1,a2,visit)
-    if cost==-1:
-        return 1e5
-    res+=cost
-    dvisit=[[0]*(M+1) for _ in range(N+1)]
-    x,y=a2
-    while True:
-        dvisit[x][y]=1
-        if [x,y]==a1:
-            break
-        x,y=visit[x][y]
-    cost=bfs(b1,b2,dvisit)
-    if cost==-1:
-        return 1e5
-    res+=cost
-    return res
-N,M=map(int,input().split())
-A1=list(map(int,input().split()))
-A2=list(map(int,input().split()))
-B1=list(map(int,input().split()))
-B2=list(map(int,input().split()))
+def K(s,e,v):
+    q=[(*s,0)];v[s[0]][s[1]]=1
+    for x,y,c in q:
+        if[x,y]==e:return c
+        for i in R(4):
+            X,Y=x+[0,0,1,-1][i],y+[1,-1,0,0][i]
+            if 0<=X<=N and 0<=Y<=M and not v[X][Y]:
+                v[X][Y]=x,y;q+=[(X,Y,c+1)]
+    return 1e5
 
-res1=func(A1,A2,B1,B2)
-res2=func(B1,B2,A1,A2)
-print("IMPOSSIBLE" if res1==1e5 and res2==1e5 else min(res1,res2))
+def f(a,b,c,d):
+    v=[[0]*(M+1)for _ in R(N+1)]
+    v[c[0]][c[1]]=v[d[0]][d[1]]=1
+    if(l:=K(a,b,v))>9e4:return 1e5
+    V=[[0]*(M+1)for _ in R(N+1)];x,y=b
+    while 1:
+        V[x][y]=1
+        if[x,y]==a:break
+        x,y=v[x][y]
+    return l+K(c,d,V)
+
+a,b=f(A,B,C,D),f(C,D,A,B)
+r=min(a,b)
+print(r if r<1e5 else"IMPOSSIBLE")
