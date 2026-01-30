76fff1308ddb952dfc30c6defe184251b215bea2
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2026-01-30 19:01:53 +0900

    [Gold V] Title: 급상승, Time: 440 ms, Memory: 260332 KB -BaekjoonHub

---
+++ 백준/Gold/23978. 급상승/급상승.py
@@ -0,0 +1,21 @@
+import sys
+input=sys.stdin.readline
+
+N,K=map(int,input().split())
+li=list(map(int,input().split()))
+st,end=0,10**10
+while st<=end:
+    mid=(st+end)//2
+    res=0
+    for i in range(1,N):
+        v=mid-(li[i]-li[i-1]-1)
+        if v>=0:
+            res+=(mid+v)*(1+mid-v)//2
+        else:
+            res+=mid*(mid+1)//2
+    res+=mid*(mid+1)//2
+    if res>=K:
+        end=mid-1
+    else:
+        st=mid+1
+print(st)
