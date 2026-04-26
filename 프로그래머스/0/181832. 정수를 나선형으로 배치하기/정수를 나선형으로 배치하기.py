50c322eaed2b00bfa23f61001526b3dda6bf795b
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2026-04-26 15:41:59 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.64 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,20 +1,3 @@
-
 def solution(n):
-    d=[(0,1),(1,0),(0,-1),(-1,0)]
-    answer = [[0]*n for _ in range(n)]
-    x,y=0,0
-    v=1
-    dir=0
-    while True:
-        while True:
-            answer[x][y]=v
-            v+=1
-            if (not 0<=x+d[dir][0]<n) or (not 0<=y+d[dir][1]<n) or answer[x+d[dir][0]][y+d[dir][1]]:
-                break
-            x,y=x+d[dir][0],y+d[dir][1]
-        if v==n**2+1:
-            return answer
-        dir=(dir+1)%4
-        x,y=x+d[dir][0],y+d[dir][1]
-
+    answer = [[]]
     return answer
