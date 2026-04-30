0983670c09e42698f7c5177bf010b2b89d47e105
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2026-04-30 14:10:54 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,7 @@
+import math
+def solution(numer1, denom1, numer2, denom2):
+    a=numer1*denom2+numer2*denom1
+    b=denom1*denom2
+    c=math.gcd(a,b)
+    print(a,b,math.gcd(a,b))
+    return [a//c,b//c]
