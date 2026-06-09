13b91c88f51501928c9436e9c136f0ffd8843d46
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2026-06-09 13:52:03 +0900

    [Passed] 행복한 수열의 개수 (67ms, 17MB)

---
+++ CodeTree/trail4/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+res=0
+
+for i in range(n):
+    x=[0,0]
+    y=[0,0]
+    xf,yf=0,0
+    for j in range(n):
+        if grid[i][j]==x[1]:
+            x[0]+=1
+        else:
+            x=[1,grid[i][j]]
+        if not xf and x[0]>=m:
+            res+=1
+            xf=1
+        if grid[j][i]==y[1]:
+            y[0]+=1
+        else:
+            y=[1,grid[j][i]]
+        if not yf and y[0]>=m:
+            res+=1
+            yf=1
+print(res)
