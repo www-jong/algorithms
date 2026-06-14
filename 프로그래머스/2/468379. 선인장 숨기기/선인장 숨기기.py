05eb5ddb56bcc76005cb8ecd9ef91251b99c84b6
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2026-06-14 14:23:44 +0900

    [level 2] Title: 선인장 숨기기, Time: 627.83 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/2/468379. 선인장 숨기기/선인장 숨기기.py
+++ 프로그래머스/2/468379. 선인장 숨기기/선인장 숨기기.py
@@ -1,25 +1,27 @@
 from collections import deque
 def solution(m, n, h, w, drops):
-    INF=len(drops)+1
-    li=[[INF]*(n) for _ in range(m)]
-    for i,j in enumerate(drops):
-        li[j[0]][j[1]]=i
-    row=[[0]*(n-w+1) for _ in range(m)]
-    for x in range(m):
+    n,m=m,n
+    li=[[10**9]*(m) for _ in range(n)]
+    for i in range(len(drops)):
+        x,y=drops[i]
+        li[x][y]=i+1
+
+    row=[[0]*(m-w+1) for _ in range(n)]
+    for x in range(n):
         q=deque()
-        for y in range(n):
-            while q and li[x][q[-1]]>=li[x][y]:
+        for y in range(m):
+            while q and li[x][q[-1]]>li[x][y]:
                 q.pop()
             q.append(y)
             if q[0]<=y-w:
                 q.popleft()
             if y>=w-1:
                 row[x][y-w+1]=li[x][q[0]]
-    
-    col=[[0]*(n-w+1) for _ in range(m-h+1)]
-    for y in range(n-w+1):
+        
+    col=[[0]*(m-w+1) for _ in range(n-h+1)]
+    for y in range(m-w+1):
         q=deque()
-        for x in range(m):
+        for x in range(n):
             while q and row[q[-1]][y]>=row[x][y]:
                 q.pop()
             q.append(x)
@@ -27,9 +29,10 @@ def solution(m, n, h, w, drops):
                 q.popleft()
             if x>=h-1:
                 col[x-h+1][y]=row[q[0]][y]
-    answer = [0,0,-1]
-    for x in range(m-h+1):
-        for y in range(n-w+1):
-            if col[x][y]>answer[2]:
-                answer=[x,y,col[x][y]]
-    return answer[:-1]
+
+    res=[0,0,-1]
+    for x in range(n-h+1):
+        for y in range(m-w+1):
+            if col[x][y]>res[2]:
+                res=[x,y,col[x][y]]
+    return res[:-1]
