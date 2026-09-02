e17a931eb89167fa4ebd7bc06cb9f720865bec9f
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2026-09-02 19:48:47 +0900

    [level 3] Title: 입국심사, Time: 271.53 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,18 +1,3 @@
 def solution(n, times):
     answer = 0
-    st,end=min(times),max(times)*n
-    
-    while st<=end:
-        mid=(st+end)//2
-        tmp=0
-        for i in times:
-            tmp+=mid//i
-            if tmp>=n:
-                break
-                
-        if tmp>=n:
-            answer=mid
-            end=mid-1
-        else:
-            st=mid+1
     return answer
