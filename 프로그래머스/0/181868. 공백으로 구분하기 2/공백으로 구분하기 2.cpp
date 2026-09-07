dcc3a04026636ca073bdb72ccd0e739bf3522265
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2026-09-07 21:32:43 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.06 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cpp
@@ -1,51 +1,15 @@
 #include <string>
-
 #include <vector>
-
-
+#include <sstream>
 
 using namespace std;
 
-
-
 vector<string> solution(string my_string) {
-
     vector<string> answer;
-
-    
-
-    for (int i = 0; i < my_string.size(); ++i) {
-
-        if (my_string[i] == ' ')
-
-            continue;
-
-        
-
-        auto it = my_string.find(" ", i);
-
-        
-
-        if (it != string::npos) {
-
-            answer.push_back(my_string.substr(i, it - i));
-
-            i = it;
-
-        }
-
-        else {
-
-            answer.push_back(my_string.substr(i, my_string.size() - i));
-
-            break;
-
-        }
-
-        
-
+    stringstream s(my_string);
+    string i;
+    while(s>>i){
+        answer.push_back(i);
     }
-
     return answer;
-
 }
