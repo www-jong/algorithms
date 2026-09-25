608a1df93774204694cafe410a61ef2335ebeaec
Jong Hyeon Won <88592432+www-jong@users.noreply.github.com>
2026-09-25 16:58:06 +0900

    [Passed] 두 직사각형 (1ms, 0MB)

---
+++ CodeTree/trail2/두 직사각형/two-rectangles.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int x1,y1,x2,y2;
+int a1,b1,a2,b2;
+int main() {
+    
+    cin >>x1>>y1>>x2>>y2;
+    cin >>a1>>b1>>a2>>b2;
+    
+    if(x2<a1||a2<x1||y2<b1||b2<y1)cout <<"nonoverlapping";
+    else cout <<"overlapping";
+    return 0;
+}
